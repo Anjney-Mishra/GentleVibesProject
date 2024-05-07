06f4011b33d5797914706992b0d09f3a0853b3a2
AnjneyMishraTesting <anjneymishra21@gmail.com>
2024-05-08 01:36:27 +0530

    optimise Location Selector

--- src/components/LocationSelect.jsx
+++ src/components/LocationSelect.jsx
@@ -1,64 +1,72 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Country, State, City } from "country-state-city";
 import { Select, SelectItem } from "@nextui-org/react";
 
 const LocationSelect = () => {
-    let countryData = Country.getAllCountries();
-  const [StateData, setStateData] = useState();
-  const [CityData, setCityData] = useState();
-  const [country, setCountry] = useState(countryData[0]);
-  const [state, setState] = useState();
-  const [city, setCity] = useState()
+  let countryData = Country.getAllCountries();
+  const [StateData, setStateData] = useState(null);
+  const [CityData, setCityData] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [state, setState] = useState(null);
+  const [city, setCity] = useState(null); 
+  
 
   useEffect(() => {
     setStateData(State.getStatesOfCountry(country?.isoCode));
   }, [country]);
 
+
   useEffect(() => {
-    StateData && setState(StateData[0]);
-  }, [StateData]);
+    setCityData(City.getCitiesOfState(country?.isoCode, state?.isoCode));
+  }, [state]);
 
-  useEffect(()=>{
-    setCityData(City.getCitiesOfState(country?.isoCode,state?.isoCode))
-  },[state])
-
-  useEffect(()=>{
-    CityData && setCity(CityData[0])
-  },[CityData])
   return (
     <>
-        <Select label="Select Country" >
-                {countryData.map((country) => (
-                  <SelectItem onClick={()=>setCountry(country)} key={country.name} value={country.name}>
-                    {country.name}
-                </SelectItem>
+      <Select label="Select Country">
+        {countryData.map((country) => (
+          <SelectItem
+            onClick={() => setCountry(country)}
+            key={country.name}
+            value={country.name}
+          >
+            {country.name}
+          </SelectItem>
+        ))}
+      </Select>
+
+      {country ? (
+        <>
+          <Select label="Select State">
+            {StateData.map((state) => (
+              <SelectItem
+                onClick={() => setState(state)}
+                key={state.name}
+                value={state.name}
+              >
+                {state.name}
+              </SelectItem>
+            ))}
+          </Select>
+          
+          {state ? (
+            <>
+              <Select label="Select city">
+                {CityData.map((city) => (
+                  <SelectItem key={city.name} value={city.name}>
+                    {city.name}
+                  </SelectItem>
                 ))}
               </Select>
-
-              {StateData ? (
-                <>
-                  <Select label="Select State">
-                    {StateData.map((state) => (
-                      <SelectItem onClick={()=>setState(state)} key={state.name} value={state.name}>
-                        {state.name}
-                      </SelectItem>
-                    ))}
-                  </Select>
-                  {CityData ? <>
-                    <Select label="Select city">
-                    {CityData.map((city) => (
-                      <SelectItem key={city.name} value={city.name}>
-                        {city.name}
-                      </SelectItem>
-                    ))}
-                  </Select>
-                  </>:<></>}
-                </>
-              ) : (
-                <></>
-              )}
+            </>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default LocationSelect
+export default LocationSelect;
